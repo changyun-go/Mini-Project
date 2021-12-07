f2d9e3dc310aa9bc84c955ed34f3c26d195e3c2d
Changyun Go <aeiou75three@gmail.com>
2021-12-07 15:21:22 +0900

    현재 월, 연도 표시
    JS를 이용해 현재 월과 연도를 화면에 표시하는 기능을 구현함.

--- 4. Calendar/script.js
+++ 4. Calendar/script.js
@@ -1,8 +1,13 @@
 let newDate = new Date();
 const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
 
 let week = days[newDate.getDay()];
 let date = newDate.getDate();
+let month = months[newDate.getMonth()];
+let year = newDate.getFullYear();
 
 document.querySelector("#week").textContent = week;
 document.querySelector("#date").textContent = date;
+document.querySelector("#month").textContent = month;
+document.querySelector("#year").textContent = year;
