3e0523d93f4f10b63226c8fea7b4930f10aa78e4
Changyun Go <aeiou75three@gmail.com>
2021-12-07 15:12:12 +0900

    현재 날짜 표시
    JS를 이용해 현재 날짜를 화면에 표시하는 기능을 구현함.

--- 4. Calendar/script.js
+++ 4. Calendar/script.js
@@ -1,7 +1,8 @@
-let date = new Date();
+let newDate = new Date();
 const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
-
-let week = days[date.getDay()];
+let week = days[newDate.getDay()];
+let date = newDate.getDate();
 
 document.querySelector("#week").textContent = week;
+document.querySelector("#date").textContent = date;
