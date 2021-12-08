9516e324c6b49703df4b91b1dcd705ad993eb4c1
Changyun Go <aeiou75three@gmail.com>
2021-12-08 22:20:30 +0900

    월 변경을 위한 좌, 우 버튼 생성
    좌, 우 버튼을 생성하여, 클릭 시 이전 달 및 다음 달이 표시되는 기능을 구현함.

--- 4. Calendar/script.js
+++ 4. Calendar/script.js
@@ -4,25 +4,52 @@ const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "
 
 let week = days[newDate.getDay()];
 let date = newDate.getDate();
-let month = months[newDate.getMonth()];
 let year = newDate.getFullYear();
-
-document.querySelector("#week").textContent = week;
-document.querySelector("#date").textContent = date;
-document.querySelector("#month").textContent = month;
-document.querySelector("#year").textContent = year;
+let selectWeek = document.querySelector("#week")
+let selectDate = document.querySelector("#date")
+let selectMonth = document.querySelector("#month")
+let selectYear = document.querySelector("#year")
 
 let firstDay = newDate.getDay() - date % 7 + 1;
 
 let table = document.querySelector("#table");
 
 let dateNum = 0;
-for(i = firstDay; i <= 6; i++){
+
+let month = newDate.getMonth();
+selectMonth.textContent = months[month];
+
+function prev(){
+    if(month === 0){
+        month = 11;
+    }
+    else{
+        month--;
+    }
+    selectMonth.textContent = months[month];
+}
+
+function next(){
+    if(month === 11){
+        month = 0;
+    }
+    else{
+        month++;
+    }
+    selectMonth.textContent = months[month];
+}
+
+selectWeek.textContent = week;
+selectDate.textContent = date;
+selectYear.textContent = year;
+
+
+for(let i = firstDay; i <= 6; i++){
     dateNum++;
     table.rows[1].cells[i].textContent = dateNum;
 }
-for(k = 2; k <= 6; k++){
-    for(j = 0; j <= 6; j++){
+for(let k = 2; k <= 6; k++){
+    for(let j = 0; j <= 6; j++){
         dateNum++;
         table.rows[k].cells[j].textContent = dateNum;
         if(dateNum === 31){
@@ -30,5 +57,7 @@ for(k = 2; k <= 6; k++){
         }
     }
 }
+
+
 
 
