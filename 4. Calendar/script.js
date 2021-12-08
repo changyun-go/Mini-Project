43e60987d4a81800aeca9fa447e1461aa7fedc71
Changyun Go <aeiou75three@gmail.com>
2021-12-09 00:08:19 +0900

    월 변경 시  날짜 업데이트 기능 적용
    버튼을 눌러 월 변경 시 해당 월에 맞는 요일과 날짜로 업데이트되는 기능을 구현함.
    *30일, 31일 구분은 아직 적용하지 않았음.

--- 4. Calendar/script.js
+++ 4. Calendar/script.js
@@ -4,45 +4,21 @@ const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "
 
 let week = days[newDate.getDay()];
 let date = newDate.getDate();
-let year = newDate.getFullYear();
+
 let selectWeek = document.querySelector("#week")
 let selectDate = document.querySelector("#date")
 let selectMonth = document.querySelector("#month")
 let selectYear = document.querySelector("#year")
-
-let firstDay = newDate.getDay() - date % 7 + 1;
-
 let table = document.querySelector("#table");
-
-let dateNum = 0;
 
 let month = newDate.getMonth();
 selectMonth.textContent = months[month];
 
-function prev(){
-    if(month === 0){
-        month = 11;
-    }
-    else{
-        month--;
-    }
-    selectMonth.textContent = months[month];
-}
-
-function next(){
-    if(month === 11){
-        month = 0;
-    }
-    else{
-        month++;
-    }
-    selectMonth.textContent = months[month];
-}
-
-selectWeek.textContent = week;
-selectDate.textContent = date;
+let year = newDate.getFullYear();
 selectYear.textContent = year;
 
+let dateNum = 0;
+let firstDay = newDate.getDay() - date % 7 + 1;
 
 for(let i = firstDay; i <= 6; i++){
     dateNum++;
@@ -57,6 +33,94 @@ for(let k = 2; k <= 6; k++){
         }
     }
 }
+
+function prev(){
+    if(month === 0){
+        month = 11;
+        year--;
+    }
+    else{
+        month--;
+    }
+    selectMonth.textContent = months[month];
+    selectYear.textContent = year;
+
+    dateNum = 0;
+    firstDay = firstDay - 30 % 7;
+    let x = 0;
+    let y = 0;
+    if(firstDay < 0){
+        firstDay = firstDay + 6;
+    }
+    for(i = 0; i < firstDay; i++){
+        table.rows[1].cells[i].textContent = null;
+    }
+    for(i = firstDay; i <= 6; i++){
+        dateNum++;
+        table.rows[1].cells[i].textContent = dateNum;
+    }
+    for(k = 2; k <= 6; k++){
+        for(j = 0; j <= 6; j++){
+            dateNum++;
+            table.rows[k].cells[j].textContent = dateNum;
+            if(dateNum === 31){
+                x = k;
+                y = j;
+                break;
+            }
+        }
+        for(let q = y; q <= 6; q++){
+            table.rows[x].cells[q].textContent = null;
+        }
+    }
+}
+
+function next(){
+    if(month === 11){
+        month = 0;
+        year++;
+    }
+    else{
+        month++;
+    }
+    selectMonth.textContent = months[month];
+    selectYear.textContent = year;
+
+    dateNum = 0;
+    firstDay = firstDay + 30 % 7;
+    let x = 0;
+    let y = 0;
+    if(firstDay > 6){
+        firstDay = firstDay - 6;
+    }
+    for(i = 0; i < firstDay; i++){
+        table.rows[1].cells[i].textContent = null;
+    }
+    for(i = firstDay; i <= 6; i++){
+        dateNum++;
+        table.rows[1].cells[i].textContent = dateNum;
+    }
+    for(k = 2; k <= 6; k++){
+        for(j = 0; j <= 6; j++){
+            dateNum++;
+            table.rows[k].cells[j].textContent = dateNum;
+            if(dateNum === 31){
+                x = k;
+                y = j;
+                break;
+            }
+        }
+        for(let q = y; q <= 6; q++){
+            table.rows[x].cells[q].textContent = null;
+        }
+    }
+}
+
+selectWeek.textContent = week;
+selectDate.textContent = date;
+
+
+
 
 
 
