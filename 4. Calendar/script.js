99c2fd63b2dbb7a3754dd427de851302792ad0b4
Changyun Go <aeiou75three@gmail.com>
2021-12-10 12:41:52 +0900

    중복 코드 함수화
    월별 마지막 날짜 판별 코드와, 날짜 텍스트 생성 코드가 중복되어 함수로 생성함.

--- 4. Calendar/script.js
+++ 4. Calendar/script.js
@@ -4,113 +4,19 @@ const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "
 
 let week = days[newDate.getDay()];
 let date = newDate.getDate();
-
 let selectWeek = document.querySelector("#week")
 let selectDate = document.querySelector("#date")
 let selectMonth = document.querySelector("#month")
 let selectYear = document.querySelector("#year")
 let table = document.querySelector("#table");
-
 let month = newDate.getMonth();
-selectMonth.textContent = months[month];
-
-selectWeek.textContent = week;
-selectDate.textContent = date;
-
 let lastDate = 0;
 let lastDay = 0;
-if(month === 3||month === 5||month === 8||month === 10){
-    lastDate = 30;
-}
-else if(month === 1){
-    lastDate = 28;
-}
-else{
-    lastDate = 31;
-}
-
-let year = newDate.getFullYear();
-selectYear.textContent = year;
-
 let dateNum = 0;
 let firstDay = newDate.getDay() - date % 7 + 1;
+let year = newDate.getFullYear();
 
-if(firstDay < 0){
-    firstDay = firstDay + 7;
-}
-else if(firstDay > 6){
-    firstDay = firstDay - 7;
-}
-for(let i = 0; i < firstDay; i++){
-    table.rows[1].cells[i].textContent = null;
-}
-for(let i = firstDay; i <= 6; i++){
-    dateNum++;
-    table.rows[1].cells[i].textContent = dateNum;
-}
-for(let i = 2; i <= 4; i++){
-    for(let j = 0; j <= 6; j++){
-        dateNum++;
-        table.rows[i].cells[j].textContent = dateNum;
-    }
-}
-lastDay = firstDay + (lastDate - 29);
-console.log(lastDay);
-
-if(lastDay > 6){
-    lastDay = lastDay - 7;
-    console.log(lastDay);
-
-    for(let i = 0; i <= 6; i++){
-        dateNum++;
-        table.rows[5].cells[i].textContent = dateNum;
-    }
-    for(let i = 0; i <= lastDay; i++){
-        dateNum++;
-        table.rows[6].cells[i].textContent = dateNum;
-    }
-    for(let i = lastDay + 1; i <= 6; i++){
-        table.rows[6].cells[i].textContent = null;
-    }
-}
-else{
-    for(let i = 0; i <= lastDay; i++){
-        dateNum++;
-        table.rows[5].cells[i].textContent = dateNum;
-    }
-    for(let i = lastDay + 1; i <= 6; i++){
-        dateNum++;
-        table.rows[5].cells[i].textContent = null;
-    }
-    for(let i = 0; i <= 6; i++){
-        table.rows[6].cells[i].textContent = null;
-    }
-}
-
-function prev(){
-    if(month === 0){
-        month = 11;
-        year--;
-    }
-    else{
-        month--;
-    }
-    selectMonth.textContent = months[month];
-    selectYear.textContent = year;
-
-    if(month === 3||month === 5||month === 8||month === 10){
-        lastDate = 30;
-    }
-    else if(month === 1){
-        lastDate = 28;
-    }
-    else{
-        lastDate = 31;
-    }
-    dateNum = 0;
-    console.log(lastDay);
-    lastDay = firstDay - 1;
-    firstDay = firstDay - (lastDate - 28);
+function dateMaker(){
     if(firstDay < 0){
         firstDay = firstDay + 7;
     }
@@ -131,10 +37,9 @@ function prev(){
         }
     }
     lastDay = firstDay + (lastDate - 29);
-    console.log(lastDay);
+
     if(lastDay > 6){
         lastDay = lastDay - 7;
-        console.log(lastDay);
 
         for(let i = 0; i <= 6; i++){
             dateNum++;
@@ -161,6 +66,44 @@ function prev(){
             table.rows[6].cells[i].textContent = null;
         }
     }
+}
+
+function lastDateCheck(){
+    if(month === 3||month === 5||month === 8||month === 10){
+        lastDate = 30;
+    }
+    else if(month === 1){
+        lastDate = 28;
+    }
+    else{
+        lastDate = 31;
+    }
+}
+
+selectWeek.textContent = week;
+selectMonth.textContent = months[month];
+selectDate.textContent = date;
+selectYear.textContent = year;
+
+lastDateCheck()
+dateMaker();
+
+function prev(){
+    if(month === 0){
+        month = 11;
+        year--;
+    }
+    else{
+        month--;
+    }
+    selectMonth.textContent = months[month];
+    selectYear.textContent = year;
+
+    lastDateCheck()
+    dateNum = 0;
+    lastDay = firstDay - 1;
+    firstDay = firstDay - (lastDate - 28);
+    dateMaker();
 }
 
 function next(){
@@ -174,66 +117,9 @@ function next(){
     selectMonth.textContent = months[month];
     selectYear.textContent = year;
 
-    if(month === 3||month === 5||month === 8||month === 10){
-        lastDate = 30;
-    }
-    else if(month === 1){
-        lastDate = 28;
-    }
-    else{
-        lastDate = 31;
-    }
+    lastDateCheck()
     dateNum = 0;
-    console.log(lastDay);
     firstDay = lastDay + 1;
-    if(firstDay < 0){
-        firstDay = firstDay + 7;
-    }
-    else if(firstDay > 6){
-        firstDay = firstDay - 7;
-    }
-    for(let i = 0; i < firstDay; i++){
-        table.rows[1].cells[i].textContent = null;
-    }
-    for(let i = firstDay; i <= 6; i++){
-        dateNum++;
-        table.rows[1].cells[i].textContent = dateNum;
-    }
-    for(let i = 2; i <= 4; i++){
-        for(let j = 0; j <= 6; j++){
-            dateNum++;
-            table.rows[i].cells[j].textContent = dateNum;
-        }
-    }
-    lastDay = firstDay + (lastDate - 29);
-    console.log(lastDay);
-
-    if(lastDay > 6){
-        lastDay = lastDay - 7;
-
-        for(let i = 0; i <= 6; i++){
-            dateNum++;
-            table.rows[5].cells[i].textContent = dateNum;
-        }
-        for(let i = 0; i <= lastDay; i++){
-            dateNum++;
-            table.rows[6].cells[i].textContent = dateNum;
-        }
-        for(let i = lastDay + 1; i <= 6; i++){
-            table.rows[6].cells[i].textContent = null;
-        }
-    }
-    else{
-        for(let i = 0; i <= lastDay; i++){
-            dateNum++;
-            table.rows[5].cells[i].textContent = dateNum;
-        }
-        for(let i = lastDay + 1; i <= 6; i++){
-            dateNum++;
-            table.rows[5].cells[i].textContent = null;
-        }
-        for(let i = 0; i <= 6; i++){
-            table.rows[6].cells[i].textContent = null;
-        }
-    }
+    dateMaker();
 }
+
