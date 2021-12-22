c31dfebc96b99e106dd317eee027f9bb86f3fc2c
changyun-go <aeiou75three@gmail.com>
2021-12-22 13:31:25 +0900

    미완료 할 일 개수 표시
    완료되지 않은 할 일의 개수를 하단에 표시하는 기능을 구현함.

--- 5. To-do/script.js
+++ 5. To-do/script.js
@@ -1,12 +1,20 @@
 const input = document.querySelector("input");
 const todo = document.querySelector("#todo");
+let countDisplay = 0;
 let preEvent = 0;
+let listCount = 0;
 
 function enter(){
     const newList = document.createElement("li");
     todo.appendChild(newList);
     newList.innerHTML = `<br> <input type='checkbox'> <span> ${input.value} </span> <button onClick='removeBtn()'>`;
     input.value = null;
+    listCount++;
+    if(listCount === 1){
+        countDisplay = document.createElement("div");
+    }
+    todo.insertBefore(countDisplay, null);
+    countDisplay.textContent = listCount;
 }
 
 todo.ondblclick = function(){
@@ -30,4 +38,10 @@ document.addEventListener("click", function(e){
 
 function removeBtn(){
     event.target.parentNode.remove();
+    listCount--;
+    countDisplay.textContent = listCount;
+    if(listCount === 0){
+        countDisplay.remove();
+    }
 }
+
