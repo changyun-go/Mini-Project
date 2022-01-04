d021d0c98159cc4a41d1087c8d8f435646506ea0
changyun-go <aeiou75three@gmail.com>
2022-01-04 14:58:26 +0900

    필드 생성
    테트리스의 필드를 생성하였음.

---
+++ 6. Tetris/script.js
@@ -0,0 +1,9 @@
+const table = document.querySelector('table');
+let newTr = 0;
+
+for(let tr = 0; tr < 20; tr++){
+    newTr = table.appendChild(document.createElement('tr'));
+    for(let td = 0; td < 10; td++){
+        newTr.appendChild(document.createElement('td'));
+    }
+}
