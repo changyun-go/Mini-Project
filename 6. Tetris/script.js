7a2b2e804abe1dea2edaf2d2bc3f070c24226b43
changyun-go <aeiou75three@gmail.com>
2022-01-06 19:10:14 +0900

    버그 해결 및 미노 회전 구현
    - 키보드 이벤트가 reload에 의해 먹히지 않는 버그를 해결함.
    - 위쪽 방향키를 누르면 미노가 시계 방향으로 회전하는 기능을 구현함.

--- 6. Tetris/script.js
+++ 6. Tetris/script.js
@@ -2,6 +2,7 @@ const table = document.querySelector('table');
 let newTr = 0;
 let x = 0;
 let y = 0;
+let p = 0;
 
 for(let tr = 0; tr < 20; tr++){
     newTr = table.appendChild(document.createElement('tr'));
@@ -10,38 +11,74 @@ for(let tr = 0; tr < 20; tr++){
     }
 }
 
-x = 4;
-y = 4;
+setInterval(function () {
+    if(sessionStorage.getItem('jsonX') === null){
+        sessionStorage.setItem('jsonX', 4);
+        sessionStorage.setItem('jsonY', 2);
+        sessionStorage.setItem('jsonP', 0);
+    }
 
+    x = JSON.parse(sessionStorage.getItem('jsonX'));
+    y = JSON.parse(sessionStorage.getItem('jsonY'));
+    p = JSON.parse(sessionStorage.getItem('jsonP'));
 
-let reload = setInterval(function () {
-    location.reload();
+    y++;
+
+    if(y > 20){
+        y = 0;
+    }
+    
+
+    sessionStorage.setItem('jsonY', JSON.stringify(y));
+
+    console.log(x,y,p);
+
+    const pos = [[x-1, y-1], [x, y-1], [x+1, y-1], [x+2, y-1], [x-1, y], [x, y], [x+1, y], [x+2, y], [x-1, y+1], [x, y+1], [x+1, y+1], [x+2, y+1], [x-1, y+2], [x, y+2], [x+1, y+2], [x+2, y+2]];
+
+    const shape = {
+        i: [pos[4], pos[5], pos[6], pos[7], ['skyblue']],
+        i_90: [pos[1], pos[5], pos[9], pos[13], ['skyblue']],
+        o: [pos[1], pos[2], pos[5], pos[6], ['yellow']],
+        t: [pos[1], pos[4], pos[5], pos[6], ['purple']],
+        t_left: [pos[1], pos[4], pos[5], pos[9], ['purple']],
+        t_right: [pos[1], pos[5], pos[6], pos[9], ['purple']],
+        t_180: [pos[4], pos[5], pos[6], pos[9], ['purple']],
+        l: [pos[4], pos[5], pos[6], pos[2], ['orange']],
+        l_left: [pos[0], pos[1], pos[5], pos[9], ['orange']],
+        l_right: [pos[1], pos[5], pos[9], pos[10], ['orange']],
+        l_180: [pos[4], pos[5], pos[6], pos[10], ['orange']],
+        j: [pos[4], pos[0], pos[5], pos[6], ['blue']],
+        j_left: [pos[1], pos[5], pos[8], pos[9], ['blue']],
+        j_right: [pos[1], pos[5], pos[9], pos[10], ['blue']],
+        j_180: [pos[4], pos[5], pos[6], pos[8], ['blue']],
+        z: [pos[0], pos[1], pos[5], pos[6], ['red']],
+        z_left: [pos[1], pos[4], pos[5], pos[8], ['red']],
+        z_right: [pos[2], pos[5], pos[6], pos[9], ['red']],
+        z_180: [pos[5], pos[6], pos[8], pos[9], ['red']]
+    }
+const s = {
+    position: 
+    [[pos[4], pos[5], pos[2], pos[1]], 
+    [pos[10], pos[6], pos[5], pos[1]],
+    [pos[4], pos[5], pos[2], pos[1]], 
+    [pos[10], pos[6], pos[5], pos[1]]],
+    color: 'green'
+}
+
+    for(let i = 0; i < 4; i++){
+        table.rows[s.position[p][i][1]-1].cells[s.position[p][i][0]].style.backgroundColor = '';
+        table.rows[s.position[p][i][1]].cells[s.position[p][i][0]].style.backgroundColor = s.color;
+    }
+console.log(s.position[p]);
 },600);
 
 
-console.log(JSON.parse(sessionStorage.getItem('jsonX')));
-console.log(JSON.parse(sessionStorage.getItem('jsonY')));;
-
-if(sessionStorage.getItem('jsonX') !== null){
-    x = JSON.parse(sessionStorage.getItem('jsonX'));
-}
-y = JSON.parse(sessionStorage.getItem('jsonY'));
-
-y++;
-
-if(y > 20){
-    y = 0;
-}
-
-sessionStorage.setItem('jsonY', JSON.stringify(y));
-
-
-// clearInterval(reload);
 
 document.addEventListener('keydown', () => {
     // 좌37 상38 우39 하40
     x = JSON.parse(sessionStorage.getItem('jsonX'));
     y = JSON.parse(sessionStorage.getItem('jsonY'));
+    p = JSON.parse(sessionStorage.getItem('jsonP'));
     if(event.keyCode === 37){
         x--;
     }
@@ -51,47 +88,20 @@ document.addEventListener('keydown', () => {
     else if(event.keyCode === 40){
         y++;
     }
+    else if(event.keyCode === 32){
+        y = 18;
+    }
+    else if(event.keyCode === 38){
+        p++;
+        if(p > 3){
+            p = 0;
+        }
+    }
     sessionStorage.setItem('jsonX', JSON.stringify(x));
     sessionStorage.setItem('jsonY', JSON.stringify(y));
+    sessionStorage.setItem('jsonP', JSON.stringify(p));
     location.reload();
 });
-
-
-
-console.log(x,y);
-
-const pos = [[x-1, y-1], [x, y-1], [x+1, y-1], [x+2, y-1], [x-1, y], [x, y], [x+1, y], [x+2, y], [x-1, y+1], [x, y+1], [x+1, y+1], [x+2, y+1], [x-1, y+2], [x, y+2], [x+1, y+2], [x+2, y+2]];
-
-const shape = {
-    i: [pos[4], pos[5], pos[6], pos[7], ['skyblue']],
-    i_90: [pos[1], pos[5], pos[9], pos[13], ['skyblue']],
-    o: [pos[1], pos[2], pos[5], pos[6], ['yellow']],
-    t: [pos[1], pos[4], pos[5], pos[6], ['purple']],
-    t_left: [pos[1], pos[4], pos[5], pos[9], ['purple']],
-    t_right: [pos[1], pos[5], pos[6], pos[9], ['purple']],
-    t_180: [pos[4], pos[5], pos[6], pos[9], ['purple']],
-    l: [pos[4], pos[5], pos[6], pos[2], ['orange']],
-    l_left: [pos[0], pos[1], pos[5], pos[9], ['orange']],
-    l_right: [pos[1], pos[5], pos[9], pos[10], ['orange']],
-    l_180: [pos[4], pos[5], pos[6], pos[10], ['orange']],
-    j: [pos[4], pos[0], pos[5], pos[6], ['blue']],
-    j_left: [pos[1], pos[5], pos[8], pos[9], ['blue']],
-    j_right: [pos[1], pos[5], pos[9], pos[10], ['blue']],
-    j_180: [pos[4], pos[5], pos[6], pos[8], ['blue']],
-    s: [pos[4], pos[5], pos[1], pos[2], ['green']],
-    s_left: [pos[0], pos[4], pos[5], pos[9], ['green']],
-    s_right: [pos[1], pos[5], pos[6], pos[10], ['green']],
-    s_180: [pos[4], pos[5], pos[9], pos[10], ['green']],
-    z: [pos[0], pos[1], pos[5], pos[6], ['red']],
-    z_left: [pos[1], pos[4], pos[5], pos[8], ['red']],
-    z_right: [pos[2], pos[5], pos[6], pos[9], ['red']],
-    z_180: [pos[5], pos[6], pos[8], pos[9], ['red']]
-}
-
-
-for(let i = 0; i < 4; i++){
-    table.rows[shape.s[i][1]].cells[shape.s[i][0]].style.backgroundColor = shape.s[4];
-}
 
 
 
