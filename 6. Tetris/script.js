e9693addc731c38d6d3c7be40b5b597ea9c46894
changyun-go <aeiou75three@gmail.com>
2022-01-07 13:51:02 +0900

    조작 버튼 추가
    화면 상에 조작 버튼을 추가하여 키보드 없이 조작이 가능하도록 구현하였음.

--- 6. Tetris/script.js
+++ 6. Tetris/script.js
@@ -1,10 +1,15 @@
 const table = document.querySelector('table');
+const leftKey = document.querySelector('#left-key');
+const rightKey = document.querySelector('#right-key');
+const downKey = document.querySelector('#down-key');
+const upKey = document.querySelector('#up-key');
 let newTr = 0;
 let x = 0;
 let y = 0;
 let p = 0;
 let randomItem = '';
 let mino = '';
+let detect = 22;
 
 const randomBox = ['i', 'o', 't', 'l', 'j', 's', 'z'];
     
@@ -36,40 +41,18 @@ setInterval(function () {
 
     y++;
 
-    if(y > 22){
+    if(y >= detect){
         random();
         y = 0;
     }
 
-
     sessionStorage.setItem('jsonY', JSON.stringify(y));
     
     console.log(x,y,p);
-    console.log(mino);
+    
     
     const pos = [[x-1, y-1], [x, y-1], [x+1, y-1], [x+2, y-1], [x-1, y], [x, y], [x+1, y], [x+2, y], [x-1, y+1], [x, y+1], [x+1, y+1], [x+2, y+1], [x-1, y+2], [x, y+2], [x+1, y+2], [x+2, y+2]];
     
-    const shape = {
-        i: [pos[4], pos[5], pos[6], pos[7], ['skyblue']],
-        i_90: [pos[1], pos[5], pos[9], pos[13], ['skyblue']],
-        o: [pos[1], pos[2], pos[5], pos[6], ['yellow']],
-        t: [pos[1], pos[4], pos[5], pos[6], ['purple']],
-        t_left: [pos[1], pos[4], pos[5], pos[9], ['purple']],
-        t_right: [pos[1], pos[5], pos[6], pos[9], ['purple']],
-        t_180: [pos[4], pos[5], pos[6], pos[9], ['purple']],
-        l: [pos[4], pos[5], pos[6], pos[2], ['orange']],
-        l_left: [pos[0], pos[1], pos[5], pos[9], ['orange']],
-        l_right: [pos[1], pos[5], pos[9], pos[10], ['orange']],
-        l_180: [pos[4], pos[5], pos[6], pos[10], ['orange']],
-        j: [pos[4], pos[0], pos[5], pos[6], ['blue']],
-        j_left: [pos[1], pos[5], pos[8], pos[9], ['blue']],
-        j_right: [pos[1], pos[5], pos[9], pos[10], ['blue']],
-        j_180: [pos[4], pos[5], pos[6], pos[8], ['blue']],
-        z: [pos[0], pos[1], pos[5], pos[6], ['red']],
-        z_left: [pos[1], pos[4], pos[5], pos[8], ['red']],
-        z_right: [pos[2], pos[5], pos[6], pos[9], ['red']],
-        z_180: [pos[5], pos[6], pos[8], pos[9], ['red']]
-    }
     const i = {
         position: 
         [[pos[7], pos[6], pos[5], pos[4]], 
@@ -125,20 +108,30 @@ setInterval(function () {
         [pos[8], pos[5], pos[4], pos[1]],
         [pos[6], pos[5], pos[1], pos[0]], 
         [pos[8], pos[5], pos[4], pos[1]]],
-        color: 'red'
+        color: 'red'      
     }
 
-    
-    
     mino = eval(randomItem);
-    
 
     for(let i = 0; i < 4; i++){
         table.rows[mino.position[p][i][1]-1].cells[mino.position[p][i][0]].style.backgroundColor = '';
         table.rows[mino.position[p][i][1]].cells[mino.position[p][i][0]].style.backgroundColor = mino.color;
     }
-},600);
 
+    let detector = 0;
+    for(let i = 0; i < 4; i++){
+        detector = table.rows[mino.position[p][0][1]+1].cells[mino.position[p][0][0]].style.backgroundColor;
+    }
+    console.log(detector);
+
+    if(detector.length > 0){
+        console.log(123);
+        console.log(y);
+        detect = y;
+        
+    }
+
+},300);
 
 
 document.addEventListener('keydown', () => {
@@ -146,30 +139,65 @@ document.addEventListener('keydown', () => {
     x = JSON.parse(sessionStorage.getItem('jsonX'));
     y = JSON.parse(sessionStorage.getItem('jsonY'));
     p = JSON.parse(sessionStorage.getItem('jsonP'));
-    if(event.keyCode === 37){
-        x--;
+
+    switch (event.keyCode) {
+        case 37:
+            x--;
+            break;
+        case 39:
+            x++;
+            break;
+        case 40:
+            y++;
+            break;
+        case 32:
+            y = 18;
+            break;
+        case 38:
+            p++;
+            if(p > 3){
+                p = 0;
+            }
+            break;
     }
-    else if(event.keyCode === 39){
-        x++;
-    }
-    else if(event.keyCode === 40){
-        y++;
-    }
-    else if(event.keyCode === 32){
-        y = 18;
-    }
-    else if(event.keyCode === 38){
-        p++;
-        if(p > 3){
-            p = 0;
-        }
-    }
+
     sessionStorage.setItem('jsonX', JSON.stringify(x));
     sessionStorage.setItem('jsonY', JSON.stringify(y));
     sessionStorage.setItem('jsonP', JSON.stringify(p));
     location.reload();
 });
 
+document.addEventListener('click', () => {
+    x = JSON.parse(sessionStorage.getItem('jsonX'));
+    y = JSON.parse(sessionStorage.getItem('jsonY'));
+    p = JSON.parse(sessionStorage.getItem('jsonP'));
+
+    switch (event.target.id) {
+        case 'left-key':
+            x--;
+            break;
+        case 'right-key':
+            x++;
+            break;
+        case 'down-key':
+            y++;
+            break;
+        case 'space':
+            y = 18;
+            break;
+        case 'up-key':
+            p++;
+            if(p > 3){
+                p = 0;
+            }
+            break;
+    }
+
+    sessionStorage.setItem('jsonX', JSON.stringify(x));
+    sessionStorage.setItem('jsonY', JSON.stringify(y));
+    sessionStorage.setItem('jsonP', JSON.stringify(p));
+    location.reload();
+});
 
 
 
